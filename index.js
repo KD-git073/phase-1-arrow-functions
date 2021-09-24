ee881d4be5734f84280d798a31d5fb2d64313a02
Kevin Digan <kd073@hotmail.com>
2021-09-23 22:22:44 -0400

    commit tree 76f818d13a2b6ced08f47c38f14b08e9ac4651fd

---
+++ index.js
@@ -0,0 +1,10 @@
+let divide = function(a,b) {
+    console.log(a / b);
+}
+divide(2000,100);
+
+const square =(param) => param ** 2;
+square(2);
+
+const add = (param1, param2) => param1 +param2;
+add(3,4);
